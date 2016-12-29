9d94044fb8520e18407afb096d599ac04cf4f2ce
Tomasz Baron <tomasz@tomasz-debian.home>
2016-12-29 16:25:10 +0100

    Changes in project structures

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-var connect = require('gulp-connect');
-var del = require('del');
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var inject = require('gulp-inject');
+var connect = require('gulp-connect'),
+	sass = require('gulp-sass'),
+	del = require('del'),
+	gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	inject = require('gulp-inject'),
+	minify = require('gulp-minify'),
+	runSequence = require('gulp-run-sequence'),
+	bowerFiles = require('main-bower-files');
 
 gulp.task('connect', function() {
 	connect.server({
@@ -13,27 +17,78 @@ gulp.task('connect', function() {
 });
 
 gulp.task('index', function () {
-  return gulp.src('index.html').pipe(inject(gulp.src(['./js/**/*.js', './css/**/*.css'], {read: false}), {addRootSlash: false}))
-    .pipe(gulp.dest('./build/'));
+  return gulp.src('index.html')
+  .pipe(inject(gulp.src(bowerFiles('**/*.min.js'), {read: false}), {name: 'libs'}))
+  //.pipe(inject(gulp.src('./build/js/libs/**/*.js', {read: false}), {addRootSlash: false, ignorePath: 'build', name: 'libs'}))
+  .pipe(inject(gulp.src(['./js/shared/**/*.js', './js/app/**/*.js', './build/css/**/*.css', './libs/angular-material/angular-material.min.css'], {read: false}), {addRootSlash: false, ignorePath: 'build'}))
+  .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('login', function () {
+  return gulp.src('login.html')
+  .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'libs'}))
+  //.pipe(inject(gulp.src('./build/js/libs/**/*.js', {read: false}), {addRootSlash: false, ignorePath: 'build', name: 'libs'}))
+  .pipe(inject(gulp.src(['./js/login/**/*.js', './js/shared/**/*.js', './build/css/**/*.css', './libs/angular-material/angular-material.min.css'], {read: false}), {addRootSlash: false, ignorePath: 'build'}))
+  .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('resource', function() {
-	return gulp.src(['./**/*', '.*','!**/*.ts', '!**/*.scss', '!bower.json', 
-    '!build', '!build/**',
-    '!db', '!db/**',
-    '!node_modules', '!node_modules/**', 
-    '!gulpfile.js', '!package.json', '!.git', '!.bowerrc', '!.gitignore'])
+	return gulp
+		.src(['./**/*', '.*',
+			'!**/*.ts', '!**/*.scss', '!bower.json', 
+			'!build', '!build/**',
+			'!db', '!db/**',
+			'!libs', '!libs/**',
+			'!node_modules', '!node_modules/**', 
+			'!gulpfile.js', '!package.json', '!.git', '!.bowerrc', '!.gitignore'])
 		.pipe(gulp.dest('./build/'));
 });
 
-gulp.task('clearBuild', function() {
-	return del('.build');
+gulp.task('libs', function() {
+	return gulp
+		.src(['libs/**/*.min.js', 
+			'!libs/angular-material/modules', '!libs/angular-material/modules/**',
+			'!libs/moment/min/locales.min.js', '!libs/moment/min/moment.min.js*',
+			'!libs/moment-timezone/builds/moment-timezone-*'])
+		.pipe(gulp.dest('./build/js/libs'));
 });
 
-gulp.task('serve', ['connect', 'resource', 'index']);
+gulp.task('css', function() {
+	return gulp
+		.src(['libs/angular-material/angular-material.min.css'])
+		.pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('clean', function() {
+	return del('build');
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./css/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('compress', function() {
+  gulp.src('./js/**/*.js')
+    .pipe(minify({
+        ext:{
+			src:'-debug.js',
+            min:'.min.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['*.combo.js', '*.min.js']
+    }))
+    .pipe(gulp.dest('package'))
+});
+
+gulp.task('serve', function() {
+  runSequence('resource', 'css', 'sass', 'index', 'login', 'connect');
+});
 
 gulp.task('default', ['serve'], function() {
-	gulp.watch(['./include/*.html', './index.html', 'login.html'], ['resource', 'index']);
-	gulp.watch(['./js/*.js', './js/**/*.js'], ['resource', 'index']);
-    gulp.watch(['./php/api.php'], ['resource', 'index']);
+	gulp.watch(['./css/**/*.scss'], ['sass', 'index']);
+	gulp.watch(['./include/*.html'], ['resource', 'index', 'login']);
+	gulp.watch(['./js/login/**/*.js', './js/app/**/*.js', './js/shared/**/*.js', './js/libs/**/*.js'], ['resource', 'index', 'login']);
+    gulp.watch(['./libs'], ['libs', 'css', 'index', 'login']);
 });
